68f380f5c694e92e6f0e10846da17bccf6932922
miguelcouteirorodrigues <miguels.rodrigues@live.com.pt>
2018-05-04 00:21:01 +0100

    refined palmtree locations

--- js/engine.js
+++ js/engine.js
@@ -162,7 +162,7 @@ class Engine {
 
         this.addPalmTrees = function() {
             palmTreePositions.push(new Point(100, 100, engine.getRandomInt(4,7)));
-            palmTreePositions.push(new Point(520, 88, engine.getRandomInt(4,7)));
+            palmTreePositions.push(new Point(518, 88, engine.getRandomInt(4,7)));
             palmTreePositions.push(new Point(130, 320, engine.getRandomInt(4,7)));
         }
 
